c255b6651b1a82ddf1775d38fb18d8d5af68a1dd
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-05-16 23:27:23 +0900

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,15 @@
-const loginInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form input")
 const loginButton = document.querySelector("#login-form button")
 
-function onLoginBtnClick(){
-    // console.log("hello",loginInput.value);
-    const username = loginInput.value;
-    if(username === ""){
-        alert("Please write your name")
-    }else if(username.length > 15){
+function onLoginBtnClick() {
+    const username= loginInput.value;
+    if(username === "") {
+        alert("Please write your name");
+    }else if(username.length > 15) {
         alert("Your name is too long.")
     }
-
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
 
 
+loginButton.addEventListener("click",onLoginBtnClick)
