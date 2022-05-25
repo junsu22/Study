f1044ec7fd3e0ff5d1d446c7f4eb4902615272d3
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-05-25 10:22:28 +0900

    Study Cleanup.

---
+++ React/Cleanup/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(function () {
+    console.log("h1 :)");
+    return function () {
+      console.log("bye :(");
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  return <h1>Hello</h1>
+}
+
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+    </div>
+  );
+}
+
+export default App;
