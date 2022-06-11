a25b0d6b70c92d6886e0018cfd0cafe6a4dbc71f
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-06-11 09:47:44 +0900

    #1.0

---
+++ Python/python1.py
@@ -0,0 +1,17 @@
+
+a_string = "like this" 
+a_number = 3
+a_float = 3.12
+a_boolean = False
+a_none = None 
+"""
+존재 하지 않는다. 없다!
+"""
+
+print(type(a_string))
+"""""
+암묵적인 약속 : 변수 이름을 길ㄹ게 지어야 할때
+단어끼리 분리되길 원한다. (snake case)
+"""""
+super_long_variable
+print(super_long_variable)
