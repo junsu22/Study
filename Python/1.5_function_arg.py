470e694c3512aa8aebc76af29c9ae89dac9ad863
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-06-12 13:09:26 +0900

    add .

--- Python/1.5_function_arg.py
+++ Python/1.5_function_arg.py
@@ -3,7 +3,7 @@ def say_hello(who):
 
 say_hello("Junsu")
 
-# 출력깂: hello junsu
+# 출력: hello junsu
 
 
 
@@ -13,4 +13,37 @@ def say_hello(who):
 # input 의 개념
 say_hello(True)
 
-#출력값 : hello True
+#출력 : hello True
+
+
+
+# default value : 원하는 인자에 default value 를 추가 할 수 있다. 
+
+
+def plus(a ,b):
+  print(a+b)
+#def  minus(a, b): #minus 에서 b의값을 주지 않았기 때문에 에러를 발생시킴.
+def  minus(a, b=0): #b의 값이 없을경우 0을 주어 에러를 해결할 수 있다.
+  print(a-b)   
+plus(2,5)   #2+5= 7
+minus(2)    #2-0 =2
+# 출력 7
+# 출력 2
+
+
+
+
+
+
+
+def say_hello(name="anonymous"):
+    print("hello", name) 
+
+say_hello()
+say_hello("Junsu") 
+
+# 출력
+# hello anonymous
+# hello junsu
+
+# 대부분의 function 들은 외부에서 input을 받아 사용한다.
