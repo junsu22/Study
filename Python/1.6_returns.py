a0f4637dd4f591f47c7e637e46397c7af3b64d5d
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-06-12 13:49:41 +0900

    add .

---
+++ Python/1.6_returns.py
@@ -0,0 +1,46 @@
+#return
+
+def p_plus(a, b):
+  print(a + b)
+
+
+
+def r_plus(a,b): # 호출
+  return a+b
+
+
+p_result = p_plus(2, 3)
+r_result = r_plus(2, 3)
+
+print(p_result, r_result)
+
+# 출력 
+# 5
+# None 5
+
+# p_result 는 None 이다.  아무값도 아니다
+# function 이 return 된 값으로 치환
+# p는 return 을 주지 않았다. 출력만 했다.
+
+
+
+
+
+
+# return 이 function 을 종료 하는
+
+def r_plus(a, b):
+    return a+b
+
+    print("slkdmkosamdkosdksokd") 
+
+r_result = r_plus(2, 4)
+
+print(r_result)
+
+
+# 출력 
+# 6
+
+# slkdmkosamdkosdksokd 는 실행 되지 않았다.
+
