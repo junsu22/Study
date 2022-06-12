0cedebc7874b3383736e767748937ae52623bdc0
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-06-12 11:49:26 +0900

    add .

---
+++ Python/1.5_function_arg.py
@@ -0,0 +1,16 @@
+def say_hello(who):
+  print("hello", who)
+
+say_hello("Junsu")
+
+# 출력깂: hello junsu
+
+
+
+#function 에 data를 주는 방법 (function에 input 을 주는것.) 
+def say_hello(who):
+  print("hello", who)
+# input 의 개념
+say_hello(True)
+
+#출력값 : hello True
