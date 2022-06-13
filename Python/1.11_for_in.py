2caac7cd7ed62d3fae1b79c4dbf008e0ca19bc8a
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-06-13 14:54:48 +0900

    add .

---
+++ Python/1.11_for_in.py
@@ -0,0 +1,31 @@
+days = ("Mon","Tue","wed","thur","fri")
+
+for day in days:s
+    if day is "wed":
+        break
+    else:
+        print(day)
+    
+
+# 출력
+
+# Mon
+# Tue
+
+
+
+#ex2.
+
+for letter in "junsuKim":
+    print(letter)
+
+# 출력
+#j
+#u
+#n
+#s
+#u
+#K
+#i
+#m
+# 
