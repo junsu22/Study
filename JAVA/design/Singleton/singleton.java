0a9e26ee4da32af4b77b85f394b52e1c45ffd397
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2022-10-08 00:28:29 +0900

    Update singleton.java

--- JAVA/design/Singleton/singleton.java
+++ JAVA/design/Singleton/singleton.java
@@ -3,7 +3,7 @@ package com.company.design.singleton;
 public class SocketClient {
 
 	private static SocketClient socketClient = null;
-// static 으로 해결
+// 	static 으로 error 해결
 //	싱글톤은 자기 자신을 객체로 가지고 있어야 함.
 	private SocketClient() {
 //	기본생성자를 통해 생성할 수 없도록 기본생성자를 private으로 막아준다.
