e777a41ab606260ea6eaed74fa7a4736316b945e
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2024-01-04 22:39:54 +0900

    Create 한개의_파일에_여러_클래스.java

---
+++ Error/한개의_파일에_여러_클래스.java
@@ -0,0 +1,22 @@
+public class First {
+/* public class 가 있는 경우 소스파일 이름이 일치해야 함.*/
+  public static void main(String[] args) {
+    }
+}
+
+class Second {		
+/* public class 가 없는 경우 다른 파일.java 사용가능하다. */
+/* 소스파일명은 public 이 붙지 않은 걸로 일치 */
+/* 소스파일명과 class 파일 명은 대소문자 까지 구분한다. 
+그러니 정확하게 일치 시켜 주어야 한다.*/  
+}
+	
+class third {		
+/* public class 가 없는 경우 다른 파일.java 사용가능하다. */
+/* 소스파일명은 public 이 붙지 않은 걸로 일치 */
+/* 소스파일명과 class 파일 명은 대소문자 까지 구분한다. 
+그러니 정확하게 일치 시켜 주어야 한다.*/  
+}
+	
+
+
