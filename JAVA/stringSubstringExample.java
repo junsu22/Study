02519bacbf29419282416342c9c69fd7380dfd16
junsu_Kim <96895747+junsu22@users.noreply.github.com>
2024-01-11 16:17:25 +0900

    Create stringSubstringExample

---
+++ JAVA/stringSubstringExample.java
@@ -0,0 +1,11 @@
+public class stringSubstringExample {
+public static void main (string[] args) {
+String ssn = "880815-1234567";
+
+String firstNum = ssn.substring(0,6);
+System.out.println(firstNum);
+
+String secondNum = ssn.substring(7)
+system.out.println(secondNum)
+}
+}
